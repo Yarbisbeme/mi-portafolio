424fe13cddd13b81abdf2a42d650e6ca5213924d
Yarbis Beltré🌴🥥 <yarbisbeme@gmail.com>
2025-08-26 10:27:27 -0400

    Hero Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="bg-gray-950 text-white">
+      <body className="bg-bg-dark">
         <Navbar />
         <main>{children}</main>
         <Footer />
